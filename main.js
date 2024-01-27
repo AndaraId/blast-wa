2a9f51233508c859ce2ada56856e3cb68d54e214
Achmad Rifa'i <janoko.java22@yahoo.com>
2024-01-27 08:33:07 +0700

    First commit

---
+++ main.js
@@ -0,0 +1,19 @@
+const dotenv = require('dotenv')
+dotenv.config()
+const { MongoStore } = require('wwebjs-mongo');
+const mongoose = require('mongoose');
+
+// Load the session data
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+    const store = new MongoStore({ mongoose: mongoose });
+    const client = new Client({
+        authStrategy: new RemoteAuth({
+            store: store,
+            backupSyncIntervalMs: 300000
+        })
+    });
+
+    client.initialize();
+});
+console.log('Cok')
+console.log(process.env.A)
